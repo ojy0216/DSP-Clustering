c12d4330d5d6e0603d89807c1b0586bc6b4168a9
ojy0216 <ojy7041@gmail.com>
2020-12-11 17:03:53 +0900

    Improved algorithm

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 from sklearn.cluster import KMeans
-from scipy import interpolate
 
 MAG = 10
 N_TOTAL = 600
@@ -11,7 +10,7 @@ MAX_CLUSTER = 10
 
 sample = [N_TOTAL // N_CLUSTER] * N_CLUSTER
 
-X, y = make_blobs(sample)
+X, _ = make_blobs(sample)
 
 sse = np.zeros(MAX_CLUSTER)
 
@@ -31,15 +30,14 @@ for i in range(1, MAX_CLUSTER + 1):
 sse /= N_TOTAL
 sse /= sse[0]
 
-grad = -np.gradient(sse)
+grad = -np.gradient(np.log(sse))
 log_grad = np.log10(grad)
-tck = interpolate.splrep(range(1, MAX_CLUSTER + 1), log_grad)
-tangent = -interpolate.splev(range(1, MAX_CLUSTER + 1), tck, der=1)
-print(tangent)
 
-mse = [(1 - slope) ** 2 for slope in tangent]  # MSE to 1
-print(mse)
-optimal_k = np.argmin(mse) + 1
+optimal_k = 1
+for k, data in enumerate(log_grad):
+    if data < np.average(log_grad):
+        optimal_k = k
+        break
 
 print("Number of cluster: ", N_CLUSTER)
 print('Optimal number of cluster: ', optimal_k)
@@ -49,11 +47,12 @@ optim_pred = optim_km.fit_predict(X)
 
 fig = plt.figure(figsize=(7, 9))
 
-gs = fig.add_gridspec(3, 2)
+gs = fig.add_gridspec(4, 2)
 ax1 = fig.add_subplot(gs[0, 0])
 ax2 = fig.add_subplot(gs[0, 1])
 ax3 = fig.add_subplot(gs[1, :])
 ax4 = fig.add_subplot(gs[2, :])
+ax5 = fig.add_subplot(gs[3, :])
 
 ax1.scatter(X[:, 0], X[:, 1], marker='.')
 ax1.set_title('Raw data (K = {})'.format(N_CLUSTER))
@@ -66,17 +65,17 @@ ax3.set_title('Normalized Sum of Squared distances of Samples (SSE)')
 ax3.grid(alpha=0.5)
 ax3.set_xticks(range(1, MAX_CLUSTER + 1))
 
-ax4.plot(range(1, MAX_CLUSTER + 1), log_grad, marker='x')
-ax4.set_title('Negative Gradient of SSE (Log10)')
-ax4.set_xlabel("Optimal K: {}".format(optimal_k))
+ax4.plot(range(1, MAX_CLUSTER + 1), np.log(sse), marker='x')
+ax4.set_title('Log of Normalized Sum of Squared distances of Samples (Log(SSE))')
 ax4.grid(alpha=0.5)
 ax4.set_xticks(range(1, MAX_CLUSTER + 1))
 
-i = 0
-for x, y in zip(range(1, MAX_CLUSTER + 1), log_grad):
-    label = round(tangent[i], 2)
-    ax4.annotate(label, (x, y))
-    i += 1
+ax5.plot(range(1, MAX_CLUSTER + 1), log_grad, marker='x')
+ax5.axhline(np.average(log_grad), color='orange')
+ax5.set_title('Log of Negative Gradient of Log(SSE)')
+ax5.set_xlabel("Optimal K: {}".format(optimal_k))
+ax5.grid(alpha=0.5)
+ax5.set_xticks(range(1, MAX_CLUSTER + 1))
 
 plt.tight_layout()
 plt.show()
